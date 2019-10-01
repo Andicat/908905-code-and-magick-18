ba23f51d7404d3b06c45690d03bc9eda8aea363b
Katerina <andicat@mail.ru>
2019-10-01 13:05:17 +0300

    move form, make modules as IIFE-function

---
+++ js/util.js
@@ -0,0 +1,24 @@
+'use strict';
+
+(function () {
+  window.coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+  window.eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+  window.fireballColors = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+
+
+  // получаем случайный элемент массива
+  window.getRandomElement = function (arr) {
+    return arr[Math.round((arr.length - 1) * Math.random())];
+  };
+
+  window.colorize = function (element, arr, input) {
+    var randomColor = window.getRandomElement(arr);
+
+    if (element.tagName.toLowerCase() === 'div') {
+      element.style.backgroundColor = randomColor;
+    } else {
+      element.style.fill = randomColor;
+    }
+    input.value = randomColor;
+  };
+})();
